29e08175916487574eff69f7e029b7f2c38510ab
Zakumi <max.zakumi@gmail.com>
2018-05-20 16:17:51 +0300

    Zakumi

---
+++ HomeWork2.java
@@ -0,0 +1,10 @@
+package com.zakumi.HomeWork;
+
+public class HomeWork2 {
+
+    public static void main(String[] args) {
+        int n = 29;
+        int sum = n / 10 + n % 10;
+        System.out.println(sum);
+    }
+}
