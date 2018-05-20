8bb2eb5773b2686f61e857ff9079b662154456f7
Zakumi <max.zakumi@gmail.com>
2018-05-20 15:45:49 +0300

    Zakumi

---
+++ HomeWork.java
@@ -0,0 +1,10 @@
+package com.zakumi.HomeWork;
+
+public class HomeWork {
+
+    public static void main(String[] args) {
+        int q = 21;
+        double w = 8;
+        System.out.println(q / w);
+    }
+}
