050f50fd610e2e5ee667ee1a70512f1fc13f52f9
Zakumi <max.zakumi@gmail.com>
2018-05-27 23:36:27 +0300

    Zakumi

--- HomeWork18.java
+++ HomeWork18.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class HomeWork18 {
     public static void main(String[] args) {
-            System.out.println("Введите слово:");
-            Scanner scanner=new Scanner(System.in);
-            String a=scanner.nextLine();
+            System.out.println("Enter FIO");
+            Scanner scanner = new Scanner(System.in);
+            String a = scanner.nextLine();
 
             for(int i = a.length()-1; i >= 0; i--){
                     System.out.print(a.charAt(i));
